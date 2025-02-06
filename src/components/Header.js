10330a422226af874112e0f3a9a4ab5dd24c5cc1
Gyasuddinali <gyasuddinali0786@gmail.com>
2025-02-06 18:39:12 +0530

    add logicc for show hide nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,14 @@ function Header() {
   const hnd=()=>{
     setShows(!shows);
   }
+  const handleLinkClick = () => {
+    setShows(false);
+  };
   return (
     <header className=" text-white bg-dark">
       <nav className="navbar navbar-expand-lg navbar-dark bg-danger fixed-top">
         <div className="container float-start">
-          <Link to="/" className="navbar-brand">
+          <Link to="/" className="navbar-brand" onClick={handleLinkClick}>
           InstentRepair
           </Link>
           <button
@@ -31,30 +34,31 @@ function Header() {
 
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse " id="navbarNav">
+          {/* <div className="collapse navbar-collapse " id="navbarNav"> */}
+          <div className={`collapse navbar-collapse ${shows ? 'show' : ''}`} id="navbarNav">
             <ul className="navbar-nav ms-auto text-white">
               <li className="nav-item">
-                <Link to="/" className="nav-link text-white">
+                <Link to="/" className="nav-link text-white" onClick={handleLinkClick}>
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/services" className="nav-link text-white">
+                <Link to="/services" className="nav-link text-white" onClick={handleLinkClick}>
                   Services
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/contact" className="nav-link text-white">
+                <Link to="/contact" className="nav-link text-white" onClick={handleLinkClick}>
                   Contact
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/about" className="nav-link text-white">
+                <Link to="/about" className="nav-link text-white" onClick={handleLinkClick}>
                   About
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/contactlist" className="nav-link text-white">
+                <Link to="/contactlist" className="nav-link text-white" onClick={handleLinkClick}>
                   Get
                 </Link>
               </li>
